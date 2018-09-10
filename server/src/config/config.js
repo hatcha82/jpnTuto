10ce3c1b00897db221c6d20986ca86bebecccb11
하창범 <hachangbeom@hachangbeom-ui-MacBook-Pro.local>
2018-09-10 21:53:38 +0900

    config

---
+++ server/src/config/config.js
@@ -0,0 +1,22 @@
+// const path = require('path')
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'DB_JPN_TYPER',
+    user: process.env.DB_USER || 'JPN_TYPER',
+    password: process.env.DB_PASS || 'JPN_TYPER',
+    options: {
+      dialect: process.env.DIALECT || 'mysql',
+      host: process.env.HOST || '0.0.0.0',
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+  }
+}
