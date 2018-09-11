1291832c89edf32909113836e15f4a7a557e10dd
하창범 <hachangbeom@hachangbeom-ui-MacBook-Pro.local>
2018-09-12 00:13:27 +0900

    dev env edited

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: '"http://ec2-54-180-83-127.ap-northeast-2.compute.amazonaws.com/:8080/"'
 }
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8080/`,
+    baseURL: `http://` + process.env.API_URL || 'localhost:8081' + `/`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
