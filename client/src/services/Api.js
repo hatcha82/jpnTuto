8e51a07f690e1617acedfee09eeb77273340f25f
하창범 <hachangbeom@hachangbeom-ui-MacBook-Pro.local>
2018-09-15 10:15:09 +0900

    fixed

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,8 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://` + process.env.API_URL || '106.10.51.131:8080' + `/`,
+    // baseURL: `http://` + process.env.API_URL || '106.10.51.131:8080' + `/`,
+    baseURL: `http://106.10.51.131:8080/`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
