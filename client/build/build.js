01227b3ef8162d3cd1bc74cae55de75de91238e1
하창범 <hachangbeom@hachangbeom-ui-MacBook-Pro.local>
2018-09-21 00:16:37 +0900

    night work

--- client/build/build.js
+++ client/build/build.js
@@ -32,7 +32,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.blue(`  API URL : http://${process.env.API_URL || '106.10.51.131'}:${process.env.API_PORT || 8080}/\n`))
+    console.log(chalk.blue(`  API URL : http://${process.env.API_URL || '106.10.51.131'}\n`))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -4,8 +4,6 @@ import store from '@/store/store'
 export default () => {
   return axios.create({
     baseURL: `http://` + process.env.API_URL,
-    // baseURL: `http://` + process.env.API_URL || '106.10.51.131:8080' + `:` + process.env.API_PORT || '8080' + `/`,
-    // baseURL: `http://localhost:8080/`,
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
