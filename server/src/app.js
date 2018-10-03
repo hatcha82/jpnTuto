d2258636107cee68a13762c18e5a631b5ca1c37b
하창범 <hachangbeom@hachangbeom-ui-MacBook-Pro.local>
2018-10-03 23:20:59 +0900

    WJWKD

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const {sequelize} = require('./models')
 const config = require('./config/config')
-
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
