a7f38f8f4d2ed5a638bbfcc44b6eb9236e558a47
Changbeom Ha <hatcha82@gmail.com>
2018-10-12 02:05:02 +0900

    google login applied
    Signed-off-by: Changbeom Ha <hatcha82@gmail.com>

--- client/src/main.js
+++ client/src/main.js
@@ -13,7 +13,6 @@ import underscore from 'vue-underscore'
 import vueVideo from 'vue-video'
 import VueVideoPlayer from 'vue-video-player'
 import VueAxios from 'vue-axios'
-import VueAuthenticate from 'vue-authenticate'
 import axios from 'axios';
 // require videojs style
 import 'video.js/dist/video-js.css'
@@ -34,22 +33,6 @@ Vue.use(BackToTop)
 Vue.use(infiniteScroll)
 Vue.use(vueVideo)
 Vue.use(VueAxios, axios)
-Vue.use(VueAuthenticate, {
-  baseUrl: 'http://localhost:8080', // Your API domain
-  
-  providers: {
-    google: {
-      clientId: '101271609028-qt8p60ktp498m87ectb1arhsq1pbvj7f.apps.googleusercontent.com',
-      redirectUri: 'http://localhost:8081/auth/google/redirect',
-      
-    },
-    github: {
-      clientId: '',
-      redirectUri: 'http://localhost:8080/auth/callback' // Your client app URL
-    }
-  }
-})
-
 sync(store, router)
 new Vue({
   render: h => h(App),
