e45b4e0c2065fa52f765cf123646988c6e73043d
Changbeom Ha <hatcha82@gmail.com>
2018-10-20 16:44:52 +0900

    git image bot
    Signed-off-by: Changbeom Ha <hatcha82@gmail.com>

--- bot/ImageGifFuriganaKanjiBot/app.js
+++ bot/ImageGifFuriganaKanjiBot/app.js
@@ -12,7 +12,7 @@ sequelize.sync({force: false})
 .then(() => {
   console.log(`DB is connected...`)
   botRun()
-  var internval = 60 * 60
+  var internval = 20
   setInterval(botRun,1000 * internval)
 })
 
@@ -22,13 +22,9 @@ var word = '日'
 async function botRun(){
   var kanjis = await Kanji.findAll({
     where: {
-      twitterUploaded: null
+      strokeOrderGifUri: null
     },
     limit : 1,
-    order: [
-      [sequelize.random()],
-      ['createdAt', 'ASC']
-    ]
   })
   
   word = kanjis[0].kanji;
@@ -39,28 +35,9 @@ async function botRun(){
     console.log('1.Jisho Ok')
     // var strokeOrderDiagramB64 = await image2base64(result.strokeOrderDiagramUri) // GIF
     // var strokeOrderSvgb64 = await image2base64(result.strokeOrderSvgUri) // GIF
-   
-    var strokeOrderGif64 = await image2base64(result.strokeOrderGifUri) // GIF
-    console.log('2.Download Ok (GIF)')
-
-    var media_ids = []
-    var uploadResult
-    // uploadResult = await client.post('media/upload', {media_data: strokeOrderDiagramB64})
-    // media_ids.push(uploadResult.media_id_string)
-    // console.log('uploadResult:')  
-    // console.log(uploadResult)  
-    // uploadResult = await client.post('media/upload', {media_data: strokeOrderSvgb64})
-    // media_ids.push(uploadResult.media_id_string)
-    // console.log('uploadResult:')  
-    // console.log(uploadResult)  
-    
-    uploadResult = await client.post('media/upload', {media_data: strokeOrderGif64})  
-    media_ids.push(uploadResult.media_id_string)
-    console.log('3.Meida Upload To tweeter')
-
     var twitText = `
 漢字 : ${result.query}
-JLPT Lev :  ${result.jlptLevel}
+JLPT Levl :  ${result.jlptLevel}
 音読 :  ${result.onyomi}
 例) 
 [[kunyomiExamples]]
@@ -87,16 +64,10 @@ JLPT Lev :  ${result.jlptLevel}
       }
     })
     twitText =  twitText.replace('[[onyomiExamples]]',template)
-    var media_ids = media_ids.join(',')
-    //
     
-    var parseResult = twitterText.parseTweet(twitText);
-    
-    var twitResult = await client.post('statuses/update', {status: twitText,media_ids:media_ids})
     console.log("Tweet!!")
     var updatedAt = new Date()
     Kanji.update({
-      twitterUploaded : 'Y',
       strokeOrderGifUri : result.strokeOrderGifUri
     }, {
       where: { id: kanjis[0].id }
