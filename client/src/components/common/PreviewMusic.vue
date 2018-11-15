90665cdfd5eac0320cb4c8f95a8c93115e18518b
Changbeom Ha <hatcha82@gmail.com>
2018-11-15 12:42:34 +0900

    git vue
    Signed-off-by: Changbeom Ha <hatcha82@gmail.com>

---
+++ client/src/components/common/PreviewMusic.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+     this
+    <audio id="previewPlayer"></audio>
+    {{src}}
+     <v-btn
+      dark
+      class="primary"
+      @click="play">      
+      <v-icon >far fa-bookmark</v-icon>
+    </v-btn>
+   </div>
+</template>
+
+<script>
+
+export default {
+  props: [
+    'src'
+  ],
+  watch:{
+     
+  },
+  methods:{
+    play () {
+      var previewPlayer = document.getElementById('previewPlayer')
+      previewPlayer.play()
+    },
+  }  
+}
+</script>
+
+<style>
+</style>
