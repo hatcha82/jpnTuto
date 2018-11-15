18f47086e46eb4fa4421fb7e40abac6b4530df37
Changbeom Ha <hatcha82@gmail.com>
2018-11-15 16:11:45 +0900

    translate random
    Signed-off-by: Changbeom Ha <hatcha82@gmail.com>

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,8 +3,8 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: `http://www.furiganahub.com:8080`, // + process.env.API_URL,
-    // baseURL: `http://localhost:8081`, // + process.env.API_URL,
+    //baseURL: `http://www.furiganahub.com:8080`, // + process.env.API_URL,
+    baseURL: `http://localhost:8081`, // + process.env.API_URL,
     headers: {
       'Access-Control-Allow-Origin': '*',
       Authorization: `Bearer ${store.state.token}`
