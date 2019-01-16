92d0fe4aa478cec8c773bf38e7113b438d8037e6
Changbeom Ha <hatcha82@gmail.com>
2019-01-16 13:20:11 +0900

    douwar translate added

---
+++ devTest/puppeteer/models/Douwa.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Douwa = sequelize.define('Douwa', {
+    episod:DataTypes.STRING, 
+    title: DataTypes.STRING,
+    titleFurigana: DataTypes.STRING,
+    titleTranslate: DataTypes.STRING,    
+    linkUrl: DataTypes.STRING,
+    ImageUrl: DataTypes.STRING,  
+    audioUrl : DataTypes.STRING,    
+    article: DataTypes.TEXT,
+    articleType : DataTypes.STRING, 
+    articelOnlyText :   DataTypes.STRING, 
+    furigana: DataTypes.TEXT,    
+    translateText: DataTypes.TEXT,
+    createdUserId: DataTypes.INTEGER,
+    updatedUserId: DataTypes.INTEGER
+  })
+
+  Douwa.associate = function (models) {
+  }
+
+  return Douwa
+}
